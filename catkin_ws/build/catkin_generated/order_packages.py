48b45ee647f3d223c635eeb4d53c1513824831b8
Lukas Petermann <lukas.petermann13@gmail.com>
2022-06-13 18:07:42 +0200

    added catkin workspace, moved latex into Document folder

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/pedda/Documents/uni/BA/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/pedda/Documents/uni/BA/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/pedda/Documents/uni/BA/catkin_ws/devel;/opt/ros/noetic' != '' else []
