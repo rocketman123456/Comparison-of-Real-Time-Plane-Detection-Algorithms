a3b96cade891eac89858eb80cfe603f61d966498
Lukas Petermann <60653355+lupeterm@users.noreply.github.com>
2022-06-15 06:18:34 +0200

    Delete catkin_ws/devel directory

--- catkin_ws/devel/share/realsense2_camera/cmake/realsense2_camera-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(realsense2_camera_MESSAGE_FILES "/home/pedda/Documents/uni/BA/catkin_ws/src/realsense-ros/realsense2_camera/msg/IMUInfo.msg;/home/pedda/Documents/uni/BA/catkin_ws/src/realsense-ros/realsense2_camera/msg/Extrinsics.msg;/home/pedda/Documents/uni/BA/catkin_ws/src/realsense-ros/realsense2_camera/msg/Metadata.msg")
-set(realsense2_camera_SERVICE_FILES "/home/pedda/Documents/uni/BA/catkin_ws/src/realsense-ros/realsense2_camera/srv/DeviceInfo.srv")
