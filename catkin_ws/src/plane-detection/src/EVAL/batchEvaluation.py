f59a4eb888575cdbea4ec66fe99adb2459a5f4e2
Lukas Petermann <lukas.petermann13@gmail.com>
2022-08-27 15:48:38 +0200

    add comments, argument handling

--- catkin_ws/src/plane-detection/src/EVAL/batchEvaluation.py
+++ catkin_ws/src/plane-detection/src/EVAL/batchEvaluation.py
@@ -1,12 +1,12 @@
+import argparse
 import matplotlib.pyplot as plt
 import os
 from typing import Dict, List
 from classes import Result
 from evaluate import evaluate
 import pandas as pd
-import numpy as np
-# TODO maybe implement some data analysis stuff using pandas and visualize it?
 
+# globals
 ALGOS = {'RSPD': "/home/pedda/Documents/uni/BA/Thesis/catkin_ws/src/plane-detection/src/EVAL/AlgoBinaries/command_line",
          'OPS': "/home/pedda/Documents/uni/BA/Thesis/catkin_ws/src/plane-detection/src/EVAL/AlgoBinaries/orientedPointSampling"}
 ALGO_ext = {'RSPD': '.geo', 'OPS': ''}
@@ -14,10 +14,11 @@ ALGO_in = {'RSPD': '.txt', 'OPS': '.pcd'}
 
 
 def get_df(results_folder: str):
+    # load results
     results = [Result.from_file(os.path.join(results_folder, file))
                for file in os.listdir(results_folder)]
     fig, axs = plt.subplots(1, len(ALGOS))
-    for ax, algo in zip(axs,ALGOS.keys()):
+    for ax, algo in zip(axs, ALGOS.keys()):
         ax.set_title(algo)
 
         # filter results by algorithm
@@ -25,10 +26,11 @@ def get_df(results_folder: str):
         # create algo dataframe
         rspd_df = pd.DataFrame(rspd).drop(columns=['detected', 'out_of'])
         rspd_df = rspd_df.rename(columns={'dataset': 'Scene Types'})
-        rspd_df.plot.bar(x='Scene Types', ax=ax)#, marker='o',label='rspd')
+        rspd_df.plot.bar(x='Scene Types', ax=ax)  # , marker='o',label='rspd')
 
     plt.ylim([0.0, 1.0])
     plt.show()
+    # TODO save fig to /$root_folder/figures
 
 
 def collect_results(root_folder: str):
@@ -40,14 +42,16 @@ def collect_results(root_folder: str):
     datasets = os.listdir(root_folder)
     for dataset in datasets:
         if os.path.isfile(os.path.join(root_folder, dataset)):
+            # skip files
             continue
         if dataset.startswith('nope_') or dataset == 'results':
-            # dataset has no GT, skipping
+            # skip non-datasets (without GT or results directory)
             continue
         results_per_scene[dataset] = []
         result_path = os.path.join(root_folder, dataset, 'results')
         scene_type = dataset.split('_')[0]
 
+        # populate result dicts, per_scene is currently unused, might be useful later
         for algorithm_resultfile in os.listdir(result_path):
             result = Result.from_file(os.path.join(
                 result_path, algorithm_resultfile))
@@ -55,6 +59,8 @@ def collect_results(root_folder: str):
             if scene_type not in results_per_algo[result.algorithm].keys():
                 results_per_algo[result.algorithm][scene_type] = []
             results_per_algo[result.algorithm][scene_type].append(result)
+
+    # calculate average results for each algorithm w.r.t scene type
     algo_results: List[Result] = []
     for algorithm, scenes_results in results_per_algo.items():
         for scene_type, scene in scenes_results.items():
@@ -72,6 +78,7 @@ def collect_results(root_folder: str):
             scene_type_average = Result(
                 scene_p, scene_r, scene_f1, scene_found, scene_all, f'{scene_type}_avg', algorithm)
             algo_results.append(scene_type_average)
+    # save results to file
     for result in algo_results:
         if 'results' not in os.listdir(rootFolder):
             os.mkdir(os.path.join(rootFolder, 'results'))
@@ -82,12 +89,11 @@ def collect_results(root_folder: str):
 
 def batch_evaluate(root_folder: str):
     datasets = os.listdir(root_folder)
-
     for dataset in datasets:
+        # ignore files, results and datasets without GT
         if os.path.isfile(os.path.join(root_folder, dataset)):
             continue
         if dataset.startswith('nope_') or dataset.startswith('results'):
-            # dataset has no GT, skipping
             continue
         dataset_path = os.path.join(root_folder, dataset)
         gt_path = os.path.join(dataset_path, "GT")
@@ -105,6 +111,7 @@ def batch_evaluate(root_folder: str):
 
 
 def create_pcd(filepath: str):
+    """OPS expects PCL file format so we are quickly going to convert the .txt file"""
     with open(filepath, "r") as inf, open(filepath.replace('.txt', '.pcd'), "w") as of:
         of.write("# .PCD v0.7 - Point Cloud Data file format\n")
         of.write("VERSION 0.7\nFIELDS x y z\n")
@@ -124,34 +131,46 @@ def create_pcd(filepath: str):
 
 
 def batch_detect(rootfolder: str, binaries_path: str) -> None:
-
     for dataset in os.listdir(rootfolder):
         dataset_path = os.path.join(rootfolder, dataset)
+        # again, ignore files, results and datasets without GT
         if not os.path.isdir(dataset_path):
             continue
         if 'nope_' in dataset or dataset == 'results':
             continue
         for algo, binary in ALGOS.items():
-            if algo == 'RSPD':
-                continue
+            # get input params for given algorithm
             cloud_file = os.path.join(
                 dataset_path, f'{dataset}{ALGO_in[algo]}')
             result_file = os.path.join(
                 dataset_path, algo, f'{dataset}{ALGO_ext[algo]}')
+            # create pcd file if needed (OPS)
             if cloud_file not in os.listdir(dataset_path):
                 create_pcd(cloud_file.replace('.pcd', '.txt'))
+            # create output folder if not already existing
+            # TODO clear directory if already exists?
             if algo not in os.listdir(dataset_path):
                 os.mkdir(os.path.join(dataset_path, algo))
+            # run PDA on dataset
             print(f'Calling {algo} on {dataset}!')
             command = f'{binary} {cloud_file} {result_file}'
             os.system(command)
 
 
 if __name__ == '__main__':
-    # rootFolder = sys.argv[1]
-    rootFolder = "/home/pedda/Documents/uni/BA/Thesis/catkin_ws/src/plane-detection/src/EVAL/Stanford3dDataset_v1.2_Aligned_Version/TEST"
-    algorithm_binaries = "/home/pedda/Documents/uni/BA/Thesis/catkin_ws/src/plane-detection/src/EVAL/AlgoBinaries"
+    fallback_root = "/home/pedda/Documents/uni/BA/Thesis/catkin_ws/src/plane-detection/src/EVAL/Stanford3dDataset_v1.2_Aligned_Version/TEST"
+    fallback_algo_binaries = "/home/pedda/Documents/uni/BA/Thesis/catkin_ws/src/plane-detection/src/EVAL/AlgoBinaries"
+
+    # input argument handling
+    parser = argparse.ArgumentParser('BatchEvaluation')
+    parser.add_argument('-R', '--root-folder', default=fallback_root, help='Path to root directory which includes datasets to be tested')
+    parser.add_argument('-A', '--algo-binaries', default=fallback_algo_binaries)
+    args = parser.parse_args()
+
+    rootFolder = args.root_folder
+    algorithm_binaries = args.algo_binaries
+
+    batch_detect(rootFolder, algorithm_binaries)
+    batch_evaluate(rootFolder)
+    collect_results(rootFolder)
     get_df(os.path.join(rootFolder, 'results'))
-    # batch_detect(rootFolder, algorithm_binaries)
-    # batch_evaluate(rootFolder)
-    # collect_results(rootFolder)
